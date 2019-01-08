07320d261f350993f505e6944705708045b12e8e
Cory Kim <corykim0829@gmail.com>
2019-01-08 20:06:11 +0900

    feat: potato move

---
+++ TNYB Project/Assets/03_Scripts/04_Cashier/potatoMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class potatoMove : MonoBehaviour {
+
+	private Rigidbody2D rb2d;
+	[SerializeField]private float forceHorizontalPower = 2.0f;
+	[SerializeField]private float forceUpPower = 2.0f;
+	[SerializeField]private float torquePower = 500.0f;
+	private int leftRight;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		rb2d = GetComponent<Rigidbody2D>();
+		
+		// Random Setting
+		leftRight = Random.Range(0, 2);
+		float randomForceHorizontalPower = Random.Range(forceHorizontalPower - 50, forceHorizontalPower + 50);
+		float randomForceUpPower = Random.Range(forceUpPower - 50, forceUpPower + 50);
+		
+		Debug.Log(leftRight);
+		if (leftRight == 0)
+		{
+			rb2d.AddForce(Vector3.left * randomForceHorizontalPower);
+			rb2d.AddTorque(torquePower);
+		}
+		else
+		{
+			rb2d.AddForce(Vector3.right * randomForceHorizontalPower);
+			rb2d.AddTorque(-torquePower);
+		}
+		
+		rb2d.AddForce(Vector3.up * randomForceUpPower);
+	}
+}
